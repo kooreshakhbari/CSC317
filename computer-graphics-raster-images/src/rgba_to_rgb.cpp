fc89b112083735dd65ca5c62b3f5927a30a5f803
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-01-13 22:09:34 -0500

    First two functions done

--- computer-graphics-raster-images/src/rgba_to_rgb.cpp
+++ computer-graphics-raster-images/src/rgba_to_rgb.cpp
@@ -1,13 +1,22 @@
 #include "rgba_to_rgb.h"
 
-void rgba_to_rgb(
-  const std::vector<unsigned char> & rgba,
-  const int & width,
-  const int & height,
-  std::vector<unsigned char> & rgb)
-{
-  rgb.resize(height*width*3);
+#include <iostream>
+using namespace std;
+
+void rgba_to_rgb(const std::vector<unsigned char> &rgba, const int &width,
+                 const int &height, std::vector<unsigned char> &rgb) {
+  rgb.resize(height * width * 3);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+  int j = 0;
+  for (int i = 0; i < rgba.size(); i++) {
+    if ((i + 1) % 4 == 0) {
+      // cout << "Skipping" << i << endl;
+      continue;
+    } else {
+      rgb[j] = rgba[i];
+      j++;
+    }
+  }
 }
--- computer-graphics-raster-images/src/write_ppm.cpp
+++ computer-graphics-raster-images/src/write_ppm.cpp
@@ -1,20 +1,34 @@
 #include "write_ppm.h"
-#include <fstream>
-#include <cassert>
-#include <iostream>
 
-bool write_ppm(
-  const std::string & filename,
-  const std::vector<unsigned char> & data,
-  const int width,
-  const int height,
-  const int num_channels)
-{
-  assert(
-    (num_channels == 3 || num_channels ==1 ) &&
-    ".ppm only supports RGB or grayscale images");
+#include <cassert>
+#include <fstream>
+#include <iostream>
+using namespace std;
+// P3
+// 3 4
+// 255
+// #the P3 means colors are in ascii, then 3 columns and 4 rows, then 255 for
+// max color, then RGB triplets
+bool write_ppm(const std::string& filename,
+               const std::vector<unsigned char>& data, const int width,
+               const int height, const int num_channels) {
+  assert((num_channels == 3 || num_channels == 1) &&
+         ".ppm only supports RGB or grayscale images");
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here:
+  ofstream file;
+  // Write the header
+  file.open(filename);
+  file << "P3" << endl;
+  file << width << " " << height << endl;
+  file << 255 << endl;
+
+  for (int i = 0; i < data.size() - 3; i += 3) {
+    file << (unsigned int)data[i] << " " << (unsigned int)data[i + 1] << " "
+         << (unsigned int)data[i + 2] << endl;
+  }
+
+  file.close();
   return false;
   ////////////////////////////////////////////////////////////////////////////
 }

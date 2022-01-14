8e0eb221d90234b847ca036ba88b72902320c732
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-01-14 02:34:05 -0500

    Reflect and Rotate done

--- computer-graphics-raster-images/src/reflect.cpp
+++ computer-graphics-raster-images/src/reflect.cpp
@@ -1,14 +1,19 @@
 #include "reflect.h"
 
-void reflect(
-  const std::vector<unsigned char> & input,
-  const int width,
-  const int height,
-  const int num_channels,
-  std::vector<unsigned char> & reflected)
-{
-  reflected.resize(width*height*num_channels);
+void reflect(const std::vector<unsigned char>& input, const int width,
+             const int height, const int num_channels,
+             std::vector<unsigned char>& reflected) {
+  reflected.resize(width * height * num_channels);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+  for (int i = 0; i < height; i++) {
+    for (int j = width - 1; j >= 0; j--) {
+      int z = width - j - 1;
+      for (int k = 0; k < num_channels; k++) {
+        reflected[k + num_channels * (z + width * i)] =
+            input[num_channels * (width * i + j) + k];
+      }
+    }
+  }
 }
--- computer-graphics-raster-images/src/rotate.cpp
+++ computer-graphics-raster-images/src/rotate.cpp
@@ -1,14 +1,22 @@
 #include "rotate.h"
 
-void rotate(
-  const std::vector<unsigned char> & input,
-  const int width,
-  const int height,
-  const int num_channels,
-  std::vector<unsigned char> & rotated)
-{
-  rotated.resize(height*width*num_channels);
+#include <iostream>
+using namespace std;
+
+void rotate(const std::vector<unsigned char>& input, const int width,
+            const int height, const int num_channels,
+            std::vector<unsigned char>& rotated) {
+  rotated.resize(height * width * num_channels);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+  for (int i = 0; i < height; i++) {
+    for (int j = width - 1; j >= 0; j--) {
+      int z = width - j - 1;
+      for (int k = 0; k < num_channels; k++) {
+        rotated[k + num_channels * (z * height + i)] =
+            input[num_channels * (width * i + j) + k];
+      }
+    }
+  }
 }

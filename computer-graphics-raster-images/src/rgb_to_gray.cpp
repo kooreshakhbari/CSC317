317d82ef52c1314cc77f3a1f6d0399e3cbf507c4
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-01-15 01:09:41 -0500

    rgb to gray done

--- computer-graphics-raster-images/src/rgb_to_gray.cpp
+++ computer-graphics-raster-images/src/rgb_to_gray.cpp
@@ -1,15 +1,20 @@
 #include "rgb_to_gray.h"
 
-void rgb_to_gray(
-  const std::vector<unsigned char> & rgb,
-  const int width,
-  const int height,
-  std::vector<unsigned char> & gray)
-{
-  gray.resize(height*width);
+#include <iostream>
+
+using namespace std;
+
+void rgb_to_gray(const std::vector<unsigned char>& rgb, const int width,
+                 const int height, std::vector<unsigned char>& gray) {
+  gray.resize(height * width);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+  int j = 0;
+  for (int i = 0; i < rgb.size(); i += 3) {
+    float sum = rgb[i] + rgb[i + 1] + rgb[i + 2];
+    float average = sum / 3;
+    gray[j] = (unsigned char)average;
+    j++;
+  }
 }
-
-

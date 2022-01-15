65364349f675d302e1e78b97ff4f968a5a6038c6
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-01-15 02:35:44 -0500

    Simulate bayer mosaic done

--- computer-graphics-raster-images/src/simulate_bayer_mosaic.cpp
+++ computer-graphics-raster-images/src/simulate_bayer_mosaic.cpp
@@ -1,13 +1,28 @@
 #include "simulate_bayer_mosaic.h"
 
-void simulate_bayer_mosaic(
-  const std::vector<unsigned char> & rgb,
-  const int & width,
-  const int & height,
-  std::vector<unsigned char> & bayer)
-{
-  bayer.resize(width*height);
+#include <iostream>
+
+using namespace std;
+void simulate_bayer_mosaic(const std::vector<unsigned char>& rgb,
+                           const int& width, const int& height,
+                           std::vector<unsigned char>& bayer) {
+  bayer.resize(width * height);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      int curr_index = (width * i + j);
+      int channel = 0;
+      // Get the color based on row and colmn being odd or even
+      if (i % 2 == 0) {
+        channel = (j % 2);
+      } else {
+        channel = 1 + (j % 2);
+      }
+
+      bayer[curr_index] = rgb[3 * curr_index + channel];
+    }
+  }
 }

008e3a168457a78cba05055e027ae31d4c4e8b97
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-01-17 23:24:34 -0500

    Fix simulate mosaic

--- computer-graphics-raster-images/src/simulate_bayer_mosaic.cpp
+++ computer-graphics-raster-images/src/simulate_bayer_mosaic.cpp
@@ -10,16 +10,30 @@ void simulate_bayer_mosaic(const std::vector<unsigned char>& rgb,
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
-
+  // Pattern is gbgb
+  //            brbr
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
       int curr_index = (width * i + j);
       int channel = 0;
       // Get the color based on row and colmn being odd or even
+      // even row, even column = b
+      // even row, odd column = g
+      // odd row, even column = g
+      // odd row, odd column = r
+
       if (i % 2 == 0) {
-        channel = (j % 2);
+        if (j % 2 == 0) {
+          channel = 2;
+        } else {
+          channel = 1;
+        }
       } else {
-        channel = 1 + (j % 2);
+        if (j % 2 == 0) {
+          channel = 1;
+        } else {
+          channel = 0;
+        }
       }
 
       bayer[curr_index] = rgb[3 * curr_index + channel];

e0c40fdd6d167fcbc1b968f8a5336979c374f4ac
Kooresh <kooresh.akhbari2@gmail.com>
2022-01-21 00:23:40 -0500

    Plane done

--- computer-graphics-ray-casting/src/first_hit.cpp
+++ computer-graphics-ray-casting/src/first_hit.cpp
@@ -7,9 +7,10 @@ bool first_hit(const Ray& ray, const double min_t,
   // Replace with your code here:
   bool intersect_found = false;
   bool valid_t_found = false;
+  Eigen::Vector3d current_hit_n;
   double current_t;
   for (int i = 0; i < objects.size(); i++) {
-    intersect_found = objects[i]->intersect(ray, min_t, t, n);
+    intersect_found = objects[i]->intersect(ray, min_t, t, current_hit_n);
 
     // Fist intersect found setup init values
     if (intersect_found && !valid_t_found) {
@@ -17,11 +18,13 @@ bool first_hit(const Ray& ray, const double min_t,
         valid_t_found = true;
         current_t = t;
         hit_id = i;
+        n = current_hit_n;
       }
     } else if (intersect_found && valid_t_found) {
       if (t < current_t && t > min_t) {
         current_t = t;
         hit_id = i;
+        n = current_hit_n;
       }
     }
   }

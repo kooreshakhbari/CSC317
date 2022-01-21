92a2313073b931013487a05bf4f728ae624c7865
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-01-20 22:01:33 -0500

    Viewing ray

--- computer-graphics-ray-casting/src/viewing_ray.cpp
+++ computer-graphics-ray-casting/src/viewing_ray.cpp
@@ -1,15 +1,24 @@
 #include "viewing_ray.h"
 
-void viewing_ray(
-  const Camera & camera,
-  const int i,
-  const int j,
-  const int width,
-  const int height,
-  Ray & ray)
-{
+#include <Eigen/Core>
+void viewing_ray(const Camera& camera, const int i, const int j,
+                 const int width, const int height, Ray& ray) {
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
-}
 
+  ray.origin = camera.e;
+
+  // U
+  ray.direction[0] = camera.width * ((i + 0.5) / width) - camera.width / 2;
+  // V
+  ray.direction[1] = camera.height * ((j + 0.5) / height) - camera.height / 2;
+  ray.direction[2] = -1 * camera.d;
+
+  Eigen::Matrix3d camera_transformation_matrix;
+  camera_transformation_matrix << camera.u, camera.v, camera.w;
+
+  Eigen::Vector3d dot_product = camera_transformation_matrix * ray.direction;
+
+  ray.direction = dot_product;
+}
--- computer-graphics-ray-casting/src/write_ppm.cpp
+++ computer-graphics-ray-casting/src/write_ppm.cpp
@@ -1,17 +1,41 @@
 #include "write_ppm.h"
-#include <fstream>
-#include <cassert>
-#include <iostream>
 
-bool write_ppm(
-  const std::string & filename,
-  const std::vector<unsigned char> & data,
-  const int width,
-  const int height,
-  const int num_channels)
-{
+#include <cassert>
+#include <fstream>
+#include <iostream>
+using namespace std;
+// P3
+// 3 4
+// 255
+// #the P3 means colors are in ascii, then 3 columns and 4 rows, then 255 for
+// max color, then RGB triplets
+bool write_ppm(const std::string& filename,
+               const std::vector<unsigned char>& data, const int width,
+               const int height, const int num_channels) {
+  assert((num_channels == 3 || num_channels == 1) &&
+         ".ppm only supports RGB or grayscale images");
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code from computer-graphics-raster-images
+  // Replace with your code here:
+  ofstream file;
+  // Write the header
+  file.open(filename);
+  file << "P3" << endl;
+  file << width << " " << height << endl;
+  file << 255 << endl;
+
+  if (num_channels == 1) {
+    for (int i = 0; i < data.size() - 3; i++) {
+      file << (int)data[i] << " " << (int)data[i] << " " << (int)data[i]
+           << endl;
+    }
+  } else {
+    for (int i = 0; i < data.size() - 3; i += 3) {
+      file << (int)data[i] << " " << (int)data[i + 1] << " " << (int)data[i + 2]
+           << endl;
+    }
+  }
+
+  file.close();
   return false;
   ////////////////////////////////////////////////////////////////////////////
 }

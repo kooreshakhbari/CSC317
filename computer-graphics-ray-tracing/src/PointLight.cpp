e3dc57e79474829d040cc3b90dac50f6e2970a6e
Kooresh <kooresh.akhbari2@gmail.com>
2022-01-27 22:03:46 -0500

    :)

--- computer-graphics-ray-tracing/src/PointLight.cpp
+++ computer-graphics-ray-tracing/src/PointLight.cpp
@@ -1,9 +1,15 @@
 #include "PointLight.h"
 
-void PointLight::direction(
-  const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
-{
+#include <math.h>
+void PointLight::direction(const Eigen::Vector3d& q, Eigen::Vector3d& d,
+                           double& max_t) const {
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here:
   ////////////////////////////////////////////////////////////////////////////
+
+  d = (this->p - q);
+
+  max_t = sqrt(pow(d[0], 2) + pow(d[1], 2) + pow(d[2], 2));
+
+  d = d.normalized();
 }
--- computer-graphics-ray-tracing/src/raycolor.cpp
+++ computer-graphics-ray-tracing/src/raycolor.cpp
@@ -1,19 +1,40 @@
 #include "raycolor.h"
-#include "first_hit.h"
+
 #include "blinn_phong_shading.h"
+#include "first_hit.h"
 #include "reflect.h"
 
-bool raycolor(
-  const Ray & ray, 
-  const double min_t,
-  const std::vector< std::shared_ptr<Object> > & objects,
-  const std::vector< std::shared_ptr<Light> > & lights,
-  const int num_recursive_calls,
-  Eigen::Vector3d & rgb)
-{
+bool raycolor(const Ray& ray, const double min_t,
+              const std::vector<std::shared_ptr<Object> >& objects,
+              const std::vector<std::shared_ptr<Light> >& lights,
+              const int num_recursive_calls, Eigen::Vector3d& rgb) {
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here:
-  rgb = Eigen::Vector3d(0,0,0);
+  double t = 0.0;
+  Eigen::Vector3d n;
+  int hit_id;
+  if (num_recursive_calls > 15) {
+    return false;
+  }
+
+  if (first_hit(ray, min_t, objects, hit_id, t, n)) {
+    // Ray reflected;
+    // reflected =
+    rgb = blinn_phong_shading(ray, hit_id, t, n, objects, lights);
+
+    Eigen::Vector3d recursive_color;
+    Ray reflected_ray;
+    reflected_ray.origin = ray.origin + t * ray.direction;
+    reflected_ray.direction = reflect(ray.direction, n);
+    if (raycolor(ray, min_t, objects, lights, num_recursive_calls + 1,
+                 recursive_color)) {
+      rgb += recursive_color;
+    }
+  } else {
+    return false;
+  }
+
+  rgb = Eigen::Vector3d(0, 0, 0);
   return false;
   ////////////////////////////////////////////////////////////////////////////
 }

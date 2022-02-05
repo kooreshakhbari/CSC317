b0228dca92ef17ab07cae6f5c0db32dfe7066ddf
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-02-05 16:51:04 -0500

    Box in Box

--- computer-graphics-bounding-volume-hierarchy/src/insert_box_into_box.cpp
+++ computer-graphics-bounding-volume-hierarchy/src/insert_box_into_box.cpp
@@ -1,11 +1,16 @@
 #include "insert_box_into_box.h"
+using namespace std;
 
-void insert_box_into_box(
-  const BoundingBox & A,
-  BoundingBox & B)
-{
+void insert_box_into_box(const BoundingBox& A, BoundingBox& B) {
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
-}
 
+  B.min_corner[0] = min(B.min_corner[0], A.min_corner[0]);
+  B.min_corner[1] = min(B.min_corner[1], A.min_corner[1]);
+  B.min_corner[2] = min(B.min_corner[2], A.min_corner[2]);
+
+  B.max_corner[0] = max(B.max_corner[0], A.max_corner[0]);
+  B.max_corner[1] = max(B.max_corner[1], A.max_corner[1]);
+  B.max_corner[2] = max(B.max_corner[2], A.max_corner[2]);
+}

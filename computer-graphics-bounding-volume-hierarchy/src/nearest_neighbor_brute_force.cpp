5cdd195dab9265e9e62dc828ea48bda11fd53adf
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-02-06 15:22:18 -0500

    nearest neigh brute

--- computer-graphics-bounding-volume-hierarchy/src/nearest_neighbor_brute_force.cpp
+++ computer-graphics-bounding-volume-hierarchy/src/nearest_neighbor_brute_force.cpp
@@ -1,15 +1,22 @@
 #include "nearest_neighbor_brute_force.h"
-#include <limits>// std::numeric_limits<double>::infinity();
 
-void nearest_neighbor_brute_force(
-  const Eigen::MatrixXd & points,
-  const Eigen::RowVector3d & query,
-  int & I,
-  double & sqrD)
-{
+#include <limits>  // std::numeric_limits<double>::infinity();
+
+void nearest_neighbor_brute_force(const Eigen::MatrixXd& points,
+                                  const Eigen::RowVector3d& query, int& I,
+                                  double& sqrD) {
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here:
-  I = -1;
-  sqrD = 0;
+
+  auto best_distance = std::numeric_limits<double>::infinity();
+
+  for (int i = 0; i < points.rows(); i++) {
+    auto distance = (points.row(i) - query).squaredNorm();
+
+    if (distance < best_distance) {
+      best_distance = distance;
+      I = i;
+    }
+  }
   ////////////////////////////////////////////////////////////////////////////
 }

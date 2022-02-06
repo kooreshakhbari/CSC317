a232782f6b5a4a5c2b9b897c25f36e8c3d74bc3c
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-02-06 15:40:24 -0500

    point box squared distance

--- computer-graphics-bounding-volume-hierarchy/src/point_box_squared_distance.cpp
+++ computer-graphics-bounding-volume-hierarchy/src/point_box_squared_distance.cpp
@@ -1,11 +1,26 @@
 #include "point_box_squared_distance.h"
-
-double point_box_squared_distance(
-  const Eigen::RowVector3d & query,
-  const BoundingBox & box)
-{
+using namespace std;
+double point_box_squared_distance(const Eigen::RowVector3d& query,
+                                  const BoundingBox& box) {
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here
-  return 0;
+  double ans = 0;
+  for (int i = 0; i < 3; i++) {
+    // Check to which corner the box is closest to.
+    double curr_corner_distance = 0;
+
+    if (query[i] > box.max_corner[i]) {
+      curr_corner_distance = pow(query[i] - box.max_corner[i], 2);
+    } else if (query[i] < box.min_corner[i]) {
+      curr_corner_distance = pow(box.min_corner[i] - query[i], 2);
+    } else {
+      // You are in the box
+      curr_corner_distance = 0;
+    }
+
+    ans += curr_corner_distance;
+  }
+
+  return ans;
   ////////////////////////////////////////////////////////////////////////////
 }

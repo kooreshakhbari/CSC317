82e37b43cf37b1150e8f31c0d1d29ca72ce8cc8d
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-02-06 23:51:58 -0500

    Fix bug

--- computer-graphics-bounding-volume-hierarchy/src/insert_triangle_into_box.cpp
+++ computer-graphics-bounding-volume-hierarchy/src/insert_triangle_into_box.cpp
@@ -8,11 +8,11 @@ void insert_triangle_into_box(const Eigen::RowVector3d& a,
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
 
-  B.min_corner[0] = min({a[0], b[0], c[0]});
-  B.min_corner[1] = min({a[1], b[1], c[1]});
-  B.min_corner[2] = min({a[2], b[2], c[2]});
+  B.min_corner[0] = min({a[0], b[0], c[0], B.min_corner[0]});
+  B.min_corner[1] = min({a[1], b[1], c[1], B.min_corner[1]});
+  B.min_corner[2] = min({a[2], b[2], c[2], B.min_corner[2]});
 
-  B.max_corner[0] = max({a[0], b[0], c[0]});
-  B.max_corner[1] = max({a[1], b[1], c[1]});
-  B.max_corner[2] = max({a[2], b[2], c[2]});
+  B.max_corner[0] = max({a[0], b[0], c[0], B.max_corner[0]});
+  B.max_corner[1] = max({a[1], b[1], c[1], B.max_corner[1]});
+  B.max_corner[2] = max({a[2], b[2], c[2], B.max_corner[2]});
 }

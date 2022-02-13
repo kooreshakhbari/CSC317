b48250e3ed03e27e937a5adbf6b839152786ff07
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-02-13 18:40:02 -0500

    First 2 files done

--- computer-graphics-meshes/src/write_obj.cpp
+++ computer-graphics-meshes/src/write_obj.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <cassert>
 #include <iostream>
-
+using namespace std;
 bool write_obj(
   const std::string & filename,
   const Eigen::MatrixXd & V,
@@ -16,5 +16,35 @@ bool write_obj(
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here:
   ////////////////////////////////////////////////////////////////////////////
-  return false;
+  ofstream file;
+  file.open(filename);
+
+  // First write the vertices
+
+  for (int i=0; i < V.rows(); i++) {
+    file << "v " << V(i, 0) << " " << V(i, 1) << " " << V(i, 2) << endl;
+  }
+
+  // Do VT (texture cords)
+  for (int i=0; i < UV.rows(); i++) {
+    file << "vt " << UV(i, 0) << " " << UV(i, 1) << endl;
+  }
+
+  // Normal vectors
+  for (int i=0; i < NV.rows(); i++) {
+    file << "vn " << NV(i, 0) << " " << V(i, 1) << " " << V(i, 2) << endl;
+  }
+
+  // Faces
+  for (int i =0; i< F.rows(); i++) {
+    file << "f ";
+    for (int j=0; j < F.cols(); j++) {
+      file << F(i, j)+1 << "/" << UF(i, j)+1 << "/" << NF(i, j)+1 << " ";
+    }
+    file << endl;
+
+  }
+  file.close();
+
+  return true;
 }

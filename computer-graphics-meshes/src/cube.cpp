0573bb925ac40897bf199f98bb2d43b143f0a2e9
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-02-14 14:19:29 -0500

    connected cube

--- computer-graphics-meshes/src/cube.cpp
+++ computer-graphics-meshes/src/cube.cpp
@@ -28,12 +28,12 @@ void cube(
   
   F.resize(6,4);
 
-  F << 0, 1, 4, 5, // Bottom
-       0, 1, 2, 3, // Left
-       4, 5, 6, 7, // Right
-       1, 3, 5, 7, // Front
-       0, 2, 4, 6, // Back
-       2, 3, 6, 7; // Top
+  F << 0, 4, 5, 1, // Bottom
+       2, 0, 1, 3, // Left
+       6, 7, 5, 4, // Right
+       7, 3, 1, 5, // Front
+       2, 6, 4, 0, // Back
+       2, 3, 7, 6; // Top
        
        
 
@@ -61,19 +61,19 @@ void cube(
 
   UF.resize(6,4);
 
-  UF << 2, 3, 4, 5, // White Bot
-        4, 5, 6, 7, // Green L
-        10, 11, 12, 13, // Orange R
-        5, 10, 7, 11, // Blue F
-        0, 1, 4, 6, // Red B
-        6, 8, 7, 9; // Yellow T 
+  UF << 2, 3, 5, 4, // White Bot
+        4, 5, 7, 6, // Green L
+        10, 11, 13, 12, // Orange R
+        5, 10, 11, 7, // Blue F
+        0, 1, 6, 4, // Red B
+        6, 8, 9, 7; // Yellow T 
 
   
   NV.resize(6,3);
 
   NV << 0, -1, 0, // Bottom WHITE 0
-        1, 0, 0, // Left GREEN 1 
-        -1, 0, 0, // Right ORANGE 2
+        -1, 0, 0, // Left GREEN 1 
+        1, 0, 0, // Right ORANGE 2
         0, 0, 1, // Front BLUE 3
         0, 0, -1, // Back RED 4
         0, 1, 0; // Top YELLOW 5

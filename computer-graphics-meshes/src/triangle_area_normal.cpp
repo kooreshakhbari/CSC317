9b786816300f42ed43ad41e418aca765d973b0e0
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-02-14 18:14:58 -0500

    TAN

--- computer-graphics-meshes/src/triangle_area_normal.cpp
+++ computer-graphics-meshes/src/triangle_area_normal.cpp
@@ -16,7 +16,7 @@ Eigen::RowVector3d triangle_area_normal(
   t1 = b.transpose() - a.transpose();
   t2 = c.transpose() - a.transpose();
 
-  Eigen::RowVector3d normal = t1.cross(t2) / 2.0;
+  Eigen::RowVector3d normal = t1.cross(t2) / t1.cross(t2).norm();
   
 
   return normal;

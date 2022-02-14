aba54188c71dcca5b67ba4f271fea61b12f332a0
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-02-13 23:46:37 -0500

    Per face N

--- computer-graphics-meshes/src/per_face_normals.cpp
+++ computer-graphics-meshes/src/per_face_normals.cpp
@@ -9,5 +9,13 @@ void per_face_normals(
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code:
   N = Eigen::MatrixXd::Zero(F.rows(),3);
+  // For each face calc the normal
+  for (int i = 0; i < F.rows(); i++) {
+    Eigen::RowVector3d a = V.row(F(i, 0));
+    Eigen::RowVector3d b = V.row(F(i, 1));
+    Eigen::RowVector3d c = V.row(F(i, 2));
+    N.row(i) << triangle_area_normal(a, b, c);
+  }
+  
   ////////////////////////////////////////////////////////////////////////////
 }

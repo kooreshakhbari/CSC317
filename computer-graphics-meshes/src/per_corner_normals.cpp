ef1242bd3941318f9a204e109c32439c2b51115d
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-02-14 01:33:53 -0500

    PCN

--- computer-graphics-meshes/src/per_corner_normals.cpp
+++ computer-graphics-meshes/src/per_corner_normals.cpp
@@ -3,7 +3,7 @@
 // Hint:
 #include "vertex_triangle_adjacency.h"
 #include <iostream>
-
+using namespace std;
 void per_corner_normals(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
@@ -14,4 +14,44 @@ void per_corner_normals(
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here:
   ////////////////////////////////////////////////////////////////////////////
+
+  // Create adjacency list
+  std::vector<std::vector<int> > VF;
+  vertex_triangle_adjacency(F, V.rows(), VF);
+
+  double threshold = cos(corner_threshold * M_PI / 180.0);
+
+  for (int i = 0; i < F.rows(); i++) {
+    Eigen::RowVector3d main_n (0,0,0);
+    Eigen::RowVector3d a = V.row(F(i, 0));
+    Eigen::RowVector3d b = V.row(F(i, 1));
+    Eigen::RowVector3d c = V.row(F(i, 2));
+    main_n = triangle_area_normal(a, b, c);
+
+    for (int j = 0; j < F.cols(); j++) {
+      Eigen::RowVector3d n (0,0,0);
+      int vertex_index = F(i, j);
+      // Loop through incident faces of this vertex to calculate their normal
+      for (int k=0; k < VF[vertex_index].size(); k++) {
+        Eigen::RowVector3d incident_n (0,0,0);
+        Eigen::RowVector3d a_comp = V.row(F(k, 0));
+        Eigen::RowVector3d b_comp = V.row(F(k, 1));
+        Eigen::RowVector3d c_comp = V.row(F(k, 2));
+        incident_n = triangle_area_normal(a, b, c);
+
+        // Check if we pass the threshold
+        if (abs(main_n.dot(incident_n)) > abs(threshold)) {
+          n += incident_n;
+        }
+      }
+
+      N.row(j + i*3) = n.normalized();
+
+
+
+
+
+    }
+  }
+
 }

4e3635379bb02a658db12504cc5b3fa19ab44b88
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-03-14 01:09:50 -0400

    working on it

--- computer-graphics-kinematics/src/euler_angles_to_transform.cpp
+++ computer-graphics-kinematics/src/euler_angles_to_transform.cpp
@@ -1,16 +1,16 @@
 #include "euler_angles_to_transform.h"
 
 Eigen::Affine3d euler_angles_to_transform(
-  const Eigen::Vector3d & xzx)
+    const Eigen::Vector3d &xzx)
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
   Eigen::Affine3d A;
-  A.matrix() << 
-    1,0,0,0,
-    0,1,0,0,
-    0,0,1,0,
-    0,0,0,1;
+  A = Eigen::AngleAxis<double>(xzx[0] * M_PI / 180.0, Eigen::Vector3d::UnitX()) * Eigen::AngleAxis<double>(xzx[0] * M_PI / 180.0, Eigen::Vector3d::UnitZ()) * Eigen::AngleAxis<double>(xzx[0] * M_PI / 180.0, Eigen::Vector3d::UnitX());
+  // A.matrix() << 1, 0, 0, 0,
+  //     0, 1, 0, 0,
+  //     0, 0, 1, 0,
+  //     0, 0, 0, 1;
   return A;
   /////////////////////////////////////////////////////////////////////////////
 }
--- computer-graphics-kinematics/src/forward_kinematics.cpp
+++ computer-graphics-kinematics/src/forward_kinematics.cpp
@@ -2,12 +2,27 @@
 #include "euler_angles_to_transform.h"
 #include <functional> // std::function
 
+Eigen::Affine3d forward_kinematic_recursive(int index, const Skeleton &skeleton, std::vector<Eigen::Affine3d, Eigen::aligned_allocator<Eigen::Affine3d>> &T)
+{
+  if (index >= skeleton.size() || index < 0)
+  {
+    return Eigen::Affine3d::Identity();
+  }
+
+  return forward_kinematic_recursive(skeleton[index].parent_index, skeleton, T) * skeleton[index].rest_T * euler_angles_to_transform(skeleton[index].xzx) * skeleton[index].rest_T.inverse();
+}
+
 void forward_kinematics(
-  const Skeleton & skeleton,
-  std::vector<Eigen::Affine3d,Eigen::aligned_allocator<Eigen::Affine3d> > & T)
+    const Skeleton &skeleton,
+    std::vector<Eigen::Affine3d, Eigen::aligned_allocator<Eigen::Affine3d>> &T)
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  T.resize(skeleton.size(),Eigen::Affine3d::Identity());
+  T.resize(skeleton.size(), Eigen::Affine3d::Identity());
+
+  for (int i = 0; i < skeleton.size(); i++)
+  {
+    T[i] = forward_kinematic_recursive(i, skeleton, T);
+  }
   /////////////////////////////////////////////////////////////////////////////
 }

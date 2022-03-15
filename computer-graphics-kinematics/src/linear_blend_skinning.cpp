36ae3fe394af359bb8feb3c489cd97305a97e8d0
Kooresh Akhbari <Kooresh.akhbari2@gmail.com>
2022-03-14 21:59:58 -0400

    Linear blend done

--- computer-graphics-kinematics/src/linear_blend_skinning.cpp
+++ computer-graphics-kinematics/src/linear_blend_skinning.cpp
@@ -7,6 +7,20 @@ void linear_blend_skinning(
     const Eigen::MatrixXd& W, Eigen::MatrixXd& U) {
     /////////////////////////////////////////////////////////////////////////////
     // Replace with your code
-    U = V;
+    // Formula is wij * tj * (vi, 1)
+    U.resize(V.rows(), V.cols());
+    for (int i = 0; i < V.rows(); i++) {
+        Eigen::Vector4d weight = Eigen::Vector4d::Zero();
+        for (int j = 0; j < skeleton.size(); j++) {
+            if (skeleton[j].weight_index == -1) {
+                continue;
+            }
+            Eigen::Vector4d v_1 = Eigen::Vector4d(V(i, 0), V(i, 1), V(i, 2), 1);
+            weight += W(i, skeleton[j].weight_index) * (T[j] * v_1);
+        }
+        U(i, 0) = weight(0);
+        U(i, 1) = weight(1);
+        U(i, 2) = weight(2);
+    }
     /////////////////////////////////////////////////////////////////////////////
 }
